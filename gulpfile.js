d2a9ebf1f57c2891fb7c159f98f4a3bfa95045e9
Unknown <Foolzo13@gmail.com>
2019-02-04 22:24:59 -0500

    .gitignore created
    gulpfile fixed and working.

--- gulpfile.js
+++ gulpfile.js
@@ -2,21 +2,21 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var browserSync = require('browser-sync').create();
 
-gulp.task('sass', function () {
-    return gulp.src('./sass/main.scss')
-    .pipe(sass())
-    .pipe(gulp.dest('./css'))
-    .pipe(browserSync.stream());
+gulp.task('sass', function() {
+    return gulp.src(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'])
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('src/css'))
+        .pipe(browserSync.stream());
 });
 
 gulp.task('serve', ['sass'], function() {
-
+    
     browserSync.init({
-        server: "./"
+        server: "./src"
     });
 
-    gulp.watch("./sass/main/scss", ['sass']);
-    gulp.watch("./*.html").on("change", browserSync.reload);
+    gulp.watch(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'], ['sass']);
+    gulp.watch("src/*.html").on('change', browserSync.reload);
 });
 
 gulp.task('default', ['serve']);
