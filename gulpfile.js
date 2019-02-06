71467c545cee5f909bb5c03de33518ec2235fabb
Noah <foolzo13@gmail.com>
2019-02-05 23:21:04 -0500

    Attempt to sync on second computer
    Gulp and gulpfile causing immense difficulties

--- gulpfile.js
+++ gulpfile.js
@@ -2,21 +2,21 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var browserSync = require('browser-sync').create();
 
-gulp.task('sass', function() {
+gulp.task('sass', gulp.series(function() {
     return gulp.src(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'])
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('src/css'))
         .pipe(browserSync.stream());
-});
+}));
 
-gulp.task('serve', ['sass'], function() {
+gulp.task('serve', gulp.series('sass', gulp.series(function() {
     
     browserSync.init({
         server: "./src"
     });
 
-    gulp.watch(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'], ['sass']);
+    gulp.watch(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'], gulp.series('sass'));
     gulp.watch("src/*.html").on('change', browserSync.reload);
-});
+})));
 
-gulp.task('default', ['serve']);
+gulp.task('default', gulp.series('serve'));
