8e560b079539fd1de04c42a2d12212ceb0bab09a
Ricky Lopez <rickyerlopez@gmail.com>
2019-02-10 18:52:32 -0500

    frontend can now send and receive data from backend in the form of JSON objects

--- tweeter/main_app/views.py
+++ tweeter/main_app/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
-# Create your views here.
-
 from django.http import HttpResponse
 from django.http import JsonResponse
 import json
+from django.views.decorators.csrf import csrf_exempt
+
+
 
 
 def index(request):
@@ -14,16 +15,31 @@ def index(request):
 def signup(request):
     return render(request, 'signup.html')
 
+
+# potential responses from the signup
+signup_responses = {
+                    0: 'success',
+                    1: 'user in use',
+                    2: 'email in use',
+                    3: 'other',
+}
+
+#TODO: fix csrf issue in frontend
+@csrf_exempt
 def signup_submit(request):
+    # verify that the submitted data meets the requirements
     if request.is_ajax() and request.method == 'POST':
+        # load the content of the response into another var
         data = json.loads(request.body)
         try:
-            email = data['email']
-            username = data['userName']
-            password = data['password']
+            # store all the passed data into a dict
+            user = {'email':data['email'], 'username':data['userName'],
+            'password':data['password']}
         except KeyError:
-            pass
+            # if for some reason the response is formed wrong
+            return JsonResponse({"status":signup_responses[3]})
 
-        render(request, 'signup.html', {"0":""})
-
-    render(request, 'signup.html', {"0":""})
+        # TODO: call the function that processes this info
+        # print the result of adding the user
+        response = 0;   # replace this with the actual response from the function
+        return JsonResponse({"status":signup_responses[response]})
