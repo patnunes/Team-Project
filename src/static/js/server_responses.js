d162a694487f3fe85162133a750d232c112272f0
Nwitz <Foolzo13@gmail.com>
2019-02-10 16:14:01 -0500

    Communication with back-end
    Cases to handle success and errors from backend

---
+++ src/static/js/server_responses.js
@@ -0,0 +1,11 @@
+
+var ServerResponses = {
+    SUCCESS: 1,
+    USER_NAME_IN_USE: 2,
+    EMAIL_IN_USE: 3,
+    properties: {
+        1: {name: "success", value: 1, code: "0"},
+        2: {name: "user in use", value: 2, code: "1"},
+        3: {name: "email in use", value: 3, code: "1"}
+    }
+};
