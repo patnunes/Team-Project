55f81e687b43c1d9f6bbcd34e5f09277d2002b1b
patnunes <pat.nunes94@gmail.com>
2019-02-12 20:45:10 -0500

    Add new user to DB, checked if the email or username already exists. Need to pass appropriate variable to create_user in order to work

--- tweeter/main_app/views.py
+++ tweeter/main_app/views.py
@@ -2,12 +2,31 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 # Create your views here.
 
+from .models import User
+
 from django.http import HttpResponse
 
+#Here for testing purposes, feel free to delete
+#user_info = {'email' : 'testemail@gmail.com', 'username' : 'user1', 'password' : '123'}
 
 def index(request):
     return render(request, 'index.html')
-
-
+	
 def signup(request):
-    return render(request, 'signup.html')
+	return render(request, 'signup.html')
+
+def create_user('''pass user_info_dictionary: in this case titled user_info'''):
+	all_users = User.objects.all()
+	#check for already existing usernames and email
+	for user in all_users:
+		if user.username == user_info['username']:
+			return 1
+		if user.email == user_info['email']:
+			return 2
+	#Passed, ready to create a new user
+	new_user = User()
+	new_user.username = user_info['username']
+	new_user.email = user_info['email']
+	new_user.password = user_info['password']
+	new_user.save()
+	return 0
