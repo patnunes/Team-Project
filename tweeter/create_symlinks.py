d7dcb44e18ff058ca049923c4e5623fb6c30a056
Ricky Lopez <rickyerlopez@gmail.com>
2019-02-13 18:06:45 -0500

    adjusted the is_admin function, issue #80

--- tweeter/create_symlinks.py
+++ tweeter/create_symlinks.py
@@ -43,16 +43,14 @@ def run_cmd(cmd, param="", dir="."):
 
 
 def is_admin():
-    try:
-        return ctypes.windll.shell32.IsUserAnAdmin()
-    except:
-        return False
 
-    if is_admin():
-        return 0
-    else:
-        # Re-run the program with admin rights
-        ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, __file__, None, 1)
+    try:
+        print("checking admin")
+        return not ctypes.windll.shell32.IsUserAnAdmin()
+    except:
+        return 1
+
+    ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, __file__, None, 1)
 
 
 main()
