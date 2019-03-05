623d2682f7d648420249725dbacc50303ac3b6ff
EspressoCode <hn.84@yahoo.com>
2019-03-05 11:16:36 -0500

    Jasmine installed, ready for tests to be written

--- tweeter/test_travisCI.py
+++ tweeter/test_travisCI.py
@@ -1,9 +1,17 @@
 import unittest
+import Pyv8
+from main_app import *
 
 class TestTravis(unittest.TestCase):
 
+# TestCase No 1:
     def test_print(self):
         print('Hello from Travis')
+
+#TestCase No 2:
+    def test_createUser(self):
+        user=False
+        self.assertEqual('user', 0)
 
 if __name__ == '__main__':
     unittest.main()
