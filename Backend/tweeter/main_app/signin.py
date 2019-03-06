d313b805a3a2895f1a089a35823add5d76d8fab8
patnunes <pat.nunes94@gmail.com>
2019-03-05 23:30:21 -0500

    deleted unnecessary folder

--- Backend/tweeter/main_app/signin.py
+++
@@ -1,9 +0,0 @@
-from .models import User
-
-def validate_user(userInfo):
-    user = User.objects.filter(username=userInfo['username'],
-            password = userInfo['password']).exists()
-	#if user does not have an account
-    if user is False:
-        return 4
-    return 0
