b1f9750de9cc0525c208af59081fd872f317e9d8
patnunes <pat.nunes94@gmail.com>
2019-03-06 12:29:53 -0500

    added get_more_tweets path

--- tweeter/main_app/urls.py
+++ tweeter/main_app/urls.py
@@ -13,4 +13,5 @@ urlpatterns = [
     path('signin_submit', views.signin_submit, name='signin_submit'),
     path('tweet_submit', views.tweet_submit, name='tweet_submit'),
     path('get_tweets', views.get_tweets, name='get_tweets'),
+    path('get_more_tweets', views.get_more_tweets, name='get_more_tweets'),
 ]  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
