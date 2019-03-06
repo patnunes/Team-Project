a799ab5dbcee2a07f929f97152142ab3b0adb51b
EspressoCode <hn.84@yahoo.com>
2019-03-06 15:05:53 -0500

    modified test1

--- tests/test_1.js
+++ tests/test_1.js
@@ -1,8 +1,14 @@
-describe('test1', ()=> {
+var counter =1;
+var description= "Test "
+
+describe(description + counter, ()=> {
     
     it('should return true', ()=> {
         var parameter=true;
         var result = testFunc(parameter);
+        console.log("Testing "+description+" "+counter );
+        counter++;
         expect(result).toBe(true);
+
     });
 });
