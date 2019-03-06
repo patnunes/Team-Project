a67259f29fdc1e06f5fdfb4d6573873f9754475e
EspressoCode <hn.84@yahoo.com>
2019-03-06 01:33:00 -0500

    kept only justForTest.js file in the list

--- my.conf.js
+++ my.conf.js
@@ -15,8 +15,8 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-      'gulpfile.js',
-      'src/static/js/*.js',
+      //'gulpfile.js',
+      'src/static/js/jsFileForTest.js',
       'tests/*.js'
     ],
 
