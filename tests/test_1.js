3c1af75190d692f342cd621ed63ec7ee5455b172
EspressoCode <hn.84@yahoo.com>
2019-03-06 01:09:07 -0500

    commenting double import in html files

--- tests/test_1.js
+++ tests/test_1.js
@@ -1,16 +1,8 @@
-// describe('test1', ()=> {
-    
-//     it('should return true', ()=> {
-//         var parameter=true;
-//         var result = testFunc(parameter);
-//         expect(result).toBe(true);
-//     });
-// });
 describe('test1', ()=> {
     
     it('should return true', ()=> {
-        // var parameter=true;
-        // var result = testFunc(parameter);
-        expect(1).toBe(1);
+        var parameter=true;
+        var result = testFunc(parameter);
+        expect(result).toBe(true);
     });
 });
