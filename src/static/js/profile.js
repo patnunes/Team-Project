67448d0b7798689943341f3aa756f93a5fe1250b
GD-DevFlow <gindulink@gmail.com>
2019-03-23 19:17:47 -0400

    Update Profile.js to clear cookie after log out

--- src/static/js/profile.js
+++ src/static/js/profile.js
@@ -2,6 +2,10 @@ function getCookie(name) {
   var match = document.cookie.match(new RegExp('(^| )' + name + '=([^;]+)'));
   if (match) return match[2];
 }
+ function delete_cookie(name) {
+   document.cookie = name + '=;expires=Thu, 01 Jan 1941 00:00:01 GMT;';
+};
+
 var username = getCookie("UserName");
 
 const replaced = document.querySelectorAll(".replacer");
@@ -14,6 +18,7 @@ for(i = 0; i < replaced.length; i++)
 $(document).ready(function() {
 
     $('#log_out').click(function(){
+        delete_cookie("UserName")
         window.location = "signin.html"
     });
 
