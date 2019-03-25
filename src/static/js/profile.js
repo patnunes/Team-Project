8ae2e58e2a2566cc2580a2d764a3ab9bcbde1302
Nwitz <Foolzo13@gmail.com>
2019-03-25 19:09:10 -0400

    Follow page functionality complete
    Front-end correctly retrieves and sets data from the backend when a user's profile page is displayed

--- src/static/js/profile.js
+++ src/static/js/profile.js
@@ -22,8 +22,9 @@ $(document).ready(function() {
 
     $('#follow_user').click(function(){
         var action = $(this).text() == "Follow" ? "follow" : "unfollow";
+        var antiAction = $(this).text() != "Follow" ? "follow" : "unfollow";
         console.log(action);
-        follow(action);
+        follow(action, antiAction);
         toggleFollowButton();
 
         // make follow request
@@ -37,11 +38,12 @@ $(document).ready(function() {
         request.userName2 = VISITING_USER;
         $.post("/get_info", JSON.stringify(request), function(m_response){
             console.log("post/like: m_response.status: " + m_response.status);
+            console.log("num-follwoing:" + m_response.num_following);
             switch (m_response.status) {
                 case ServerResponses.SUCCESS:
-                
+                    console.log("num-follwoing:" + m_response.num_followers);
                     setFollowButton(m_response.following);
-                    setFollowersCount(m_response.num_following);
+                    setFollowersCount(m_response.num_followers);
                     break;
                 case ServerResponses.OTHER:
                     alert("failed getting info");
@@ -60,29 +62,22 @@ $(document).ready(function() {
 
     
 
-    var follow = function(action){
+    var follow = function(action, antiAction){
         var request = new Object;
         request.userName1 = ACTIVE_USER;
         request.userName2 = VISITING_USER;
         request.action = action;
+        setFollowers(action);
         console.log("post/follow: action: " + (action));
         $.post("/follow", JSON.stringify(request), function(m_response){
             console.log("post/like: m_response.status: " + m_response.status);
             switch (m_response.status) {
                 case ServerResponses.SUCCESS:
-                    if (action == "follow") {
-                        $("#followerCounter").text(function(i, text){
-                            return (parseInt(text) + 1);
-                        })
-                    } else {
-                        $("#followerCounter").text(function(i, text){
-                            return (parseInt(text) - 1);
-                        })
-                    }
                     break;
                 case ServerResponses.OTHER:
                     alert("failed following User")
                     toggleFollowButton(); // undo the follow
+                    setFollowers(antiAction);
                     break;
                 default:
                     // DEBUG: alert("Uknown Error");
@@ -98,6 +93,18 @@ $(document).ready(function() {
             $("#follow_user").addClass("btn-primary");
             $("#follow_user").removeClass("btn-outline-primary");
             $("#follow_user").text("Following")
+        }
+    }
+
+    var setFollowers = function(action){
+        if (action == "follow") {
+            $("#followerCounter").text(function(i, text){
+                return (parseInt(text) + 1);
+            })
+        } else {
+            $("#followerCounter").text(function(i, text){
+                return (parseInt(text) - 1);
+            })
         }
     }
 
